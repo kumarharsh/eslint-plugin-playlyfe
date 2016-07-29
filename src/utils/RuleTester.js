74775b89a571dc4f20654cc088d5da01676cf217
Mayank Agarwal <mayank@playlyfe.com>
2016-07-30 02:39:00 +0530

    chore(test): replace mocha + nyc with jest
    trying jest test runner

--- src/utils/RuleTester.js
+++ src/utils/RuleTester.js
@@ -1,13 +1,2 @@
 import { RuleTester } from 'eslint';
-
-// HACK to fix --watch mocha
-// issue: when mocha watching file
-// it will clean require.cache for test files only
-// eslint RuleTester is external file
-// and in each watch run new describe and it
-// are passed but RuleTester is using previous test run
-// descibe and it
-RuleTester.describe = describe;
-RuleTester.it = it;
-
 export default RuleTester;
